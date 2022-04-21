5b967a468883c4c4f9a115d757f1d291ec7f5d26
koala117 <cassieralexis@gmail.com>
2022-04-21 14:04:17 +0200

    ajout panier

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
 
 @Component({
   selector: 'app-basket',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basket.component.scss']
 })
 export class BasketComponent implements OnInit {
-
+articles: Article[] = [new Article(1,'jojo','https://oniri-creations.com/wp-content/uploads/2022/02/ichigo-statue-oniri-creations.jpg', {name:'l', price:10}),new Article(1,'jojo','https://oniri-creations.com/wp-content/uploads/2022/02/ichigo-statue-oniri-creations.jpg', {name:'l', price:10}),new Article(1,'jojo','https://oniri-creations.com/wp-content/uploads/2022/02/ichigo-statue-oniri-creations.jpg', {name:'l', price:10})];
   constructor() {}
 
   ngOnInit(): void {
